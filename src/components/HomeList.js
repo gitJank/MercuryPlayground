66815f9df6d03b25a9546ff955a1374b2ff22fb1
Justin Jankiewicz <justinjankiewicz@gmail.com>
2018-05-16 19:48:15 -0500

    added styling

--- src/components/HomeList.js
+++ src/components/HomeList.js
@@ -5,7 +5,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import "./ComponentStyles/buyList.scss";
+import "./ComponentStyles/homeList.scss";
 
 
 
@@ -35,15 +35,16 @@ class HomeList extends Component {
                                     image={home.media}
                                     title="Beach"
                                 />
-                                <CardContent>
+                                <CardContent className="cardContent">
                                     <Typography gutterBottom variant="headline" component="h2">
-                                        {home.address}
-                                    </Typography>
-                                    <Typography component="p">
+                                        {home.address} <br/>
                                         {home.state}, {home.zipcode}
                                     </Typography>
+                                    <Typography component="p">
+                                        Price: {home.price}
+                                    </Typography>
                                 </CardContent>
-                                <CardActions>
+                                <CardActions disableActionSpacing="true">
                                     <Button size="small" color="primary">
                                         Learn More
                                     </Button>
@@ -52,8 +53,8 @@ class HomeList extends Component {
                         </div>
                     )
                 })
-                this.setState({ homes: homes });
-            })
+            this.setState({ homes: homes });
+        })
     }
 
     render() {
